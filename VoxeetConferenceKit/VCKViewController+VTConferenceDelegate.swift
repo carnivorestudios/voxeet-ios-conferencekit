76873ff48a36e5b8ba30c0785018a62daf9ae284
anuraag mohanty <anuraagmohanty@anuraags-mbp.gateway.sonic.net>
2019-07-22 16:13:03 -0700

    reverted changes to call label

--- VoxeetConferenceKit/VCKViewController+VTConferenceDelegate.swift
+++ VoxeetConferenceKit/VCKViewController+VTConferenceDelegate.swift
@@ -16,9 +16,7 @@ extension VCKViewController: VTConferenceDelegate {
             conferenceStartTimer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(conferenceStart), userInfo: nil, repeats: false)
             let title = VoxeetSDK.shared.conference.alias ?? "Unnamed"
             let endIndex = title.firstIndex(of: ":")
-            self.viewDidLoad()
             self.callNameLabel.text = String(title[..<endIndex!])
-            self.enableButtons(areEnabled: true)
         } else {
             // Update user's audio position to listen each users clearly in a 3D environment.
             updateUserPosition()
