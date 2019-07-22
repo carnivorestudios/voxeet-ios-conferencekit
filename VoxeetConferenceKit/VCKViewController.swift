502102dbdaad89cac5a00e1a2c14d2fce116275a
anuraag mohanty <anuraagmohanty@anuraags-mbp.gateway.sonic.net>
2019-07-22 12:44:59 -0700

    decreased auto disconnect time from network loss

--- VoxeetConferenceKit/VCKViewController.swift
+++ VoxeetConferenceKit/VCKViewController.swift
@@ -531,7 +531,7 @@ class VCKViewController: UIViewController {
                 self.conferenceTimerLabel.text = String(format: "%02.0f:%02.0f", floor(minute), floor(second))
             }
         }
-        if (!(NetworkStatus.shared.isReachable) && floor(date) > 10) {
+        if (!(NetworkStatus.shared.isReachable) && floor(date) > 7) {
             self.conferenceTimer?.invalidate()
             let alertController = UIAlertController(title: "Error: Connection Lost", message: "Unable to connect to internet", preferredStyle: UIAlertController.Style.alert)
             alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
