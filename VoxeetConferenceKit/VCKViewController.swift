b0da456c119f4049efcea27a8d11bbfc98ff246c
Anuraag Mohanty <anuraag@carnivorestudios.com>
2019-07-25 17:04:51 -0700

    removed unnecessary additions

--- VoxeetConferenceKit/VCKViewController.swift
+++ VoxeetConferenceKit/VCKViewController.swift
@@ -370,8 +370,6 @@ class VCKViewController: UIViewController {
     }
     
     func hangUpWithAlert() {
-        self.mainAvatarContainer.isHidden = true
-        self.usersCollectionView.isHidden = true
         let alertController = UIAlertController(title: "Error: Connection Lost", message: "Unable to connect to internet", preferredStyle: UIAlertController.Style.alert)
         self.present(alertController, animated: true, completion: nil)
         // Block hang up action if the hangUpTimer if currently active.
