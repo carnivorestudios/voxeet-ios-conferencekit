23b07d56c498c33487494e46257e71b66b8b5c67
Anuraag Mohanty <anuraag@carnivorestudios.com>
2019-08-07 11:06:10 -0700

    screenshare attach changed

--- VoxeetConferenceKit/VCKViewController.swift
+++ VoxeetConferenceKit/VCKViewController.swift
@@ -636,7 +636,8 @@ class VCKViewController: UIViewController {
             if let screenStream = screenStream, userID == screenShareUserID {
                 mainVideoRenderer.isHidden = true
                 screenShareVideoRenderer.isHidden = false
-                VoxeetSDK.shared.conference.attachMediaStream(screenStream, renderer: screenShareVideoRenderer)
+                screenShareVideoRenderer.attach(userID: userID, stream: screenStream)
+                //VoxeetSDK.shared.conference.attachMediaStream(screenStream, renderer: screenShareVideoRenderer)
             } else if let stream = stream {
                 mainVideoRenderer.isHidden = false
                 screenShareVideoRenderer.isHidden = true
